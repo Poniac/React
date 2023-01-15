87389c04974d863f8cfaa4a3c1ac114e799d91c7
Poniac <113772161+Poniac@users.noreply.github.com>
2023-01-15 14:03:38 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading">My Favourite Foods</h1>
+    <img alt="random" src={img} />
+
+    <img
+      className="food-img"
+      alt="bacon"
+      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"
+    />
+    <img
+      className="food-img"
+      alt="jamon"
+      src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"
+    />
+    <img
+      className="food-img"
+      alt="noodles"
+      src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"
+    />
+  </div>,
+  document.getElementById("root")
+);
